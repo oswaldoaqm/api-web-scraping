f527aab03568925bac3fc6e898a754a3b579ef9a
oswaldoaqm <148845466+oswaldoaqm@users.noreply.github.com>
2026-06-21 20:14:35 -0500

    Enhance web scraping with User-Agent and row handling
    Added User-Agent header to HTTP request and improved table row extraction logic.

--- scrap_table.py
+++ scrap_table.py
@@ -7,19 +7,24 @@ def lambda_handler(event, context):
     # URL de la página web que contiene la tabla
     url = "https://ultimosismo.igp.gob.pe/productos/reportes-sismicos"
 
+    # Me lo recomendo porque me bloqueaban
+    headers_req = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36'
+    }
+
     # Realizar la solicitud HTTP a la página web
-    response = requests.get(url)
+    response = requests.get(url, headers=headers_req)
     if response.status_code != 200:
         return {
             'statusCode': response.status_code,
-            'body': 'Error al acceder a la página web'
+            'body': 'Error al acceder a la página web del IGP'
         }
 
     # Parsear el contenido HTML de la página web
     soup = BeautifulSoup(response.content, 'html.parser')
 
     # Encontrar la tabla en el HTML
-    table = soup.find('table', class_='w-full border-collapse')
+    table = soup.find('table')
     if not table:
         return {
             'statusCode': 404,
@@ -35,21 +40,30 @@ def lambda_handler(event, context):
         headers = ['#', 'Cod reporte', 'Referencia', 'Magnitud', 'Fecha y hora', 'Accion']
 
     # Agarro los 10 ultimos sismos
-    tbody = table.find('tbody')
     rows = []
+    tbody = table.find('tbody')
     
-    if tbody:
-        for row in tbody.find_all('tr')[:10]:
-            cells = row.find_all('td')
-            if cells:
-                row_data = {}
-                row_data['id'] = str(uuid.uuid4())
-                
-                for i, cell in enumerate(cells):
-                    key = headers[i] if i < len(headers) else f'Columna_{i}'
-                    row_data[key] = cell.text.strip()
-                
-                rows.append(row_data)
+    # El parser no encuentra el tbody así que itero por tr
+    filas_html = tbody.find_all('tr') if tbody else table.find_all('tr')[1:]
+    
+    # Tomamos solo las primeras 10
+    for row in filas_html[:10]:
+        cells = row.find_all('td')
+        if cells:
+            row_data = {}
+            row_data['id'] = str(uuid.uuid4())
+            
+            for i, cell in enumerate(cells):
+                key = headers[i] if i < len(headers) else f'Columna_{i}'
+                row_data[key] = cell.text.strip()
+            
+            rows.append(row_data)
+
+    if len(rows) == 0:
+        return {
+            'statusCode': 404,
+            'body': 'Se encontró la tabla, pero estaba vacía (sin etiquetas <td>).'
+        }
 
     # Guardo los datos en DynamoDB
     dynamodb = boto3.resource('dynamodb')
